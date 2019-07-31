e67ff00390330dc69e8a0e78969f24652631932a
Olabamipe Taiwo <teeola48@gmail.com>
2019-07-31 21:40:18 +0000

    Final Staic UI IMplementation

--- js/main.js
+++ js/main.js
@@ -4,27 +4,28 @@
             // wait until window is loaded - meaning all images, stylesheets, js, fonts, media assets, and links
              window.addEventListener("load", function(e) {
 
-            //         document.body.style.display = 'block';
-            //     var tl = new TimelineMax();
+                  document.body.style.display = 'block';
+                  var tl = new TimelineMax();
 
-            //     tl.staggerFrom('section', 2, {
-            //     opacity: 0,
-            //     scale: .5,
-            //     ease: Power2.easeOut
-            //     }, 0.2)
+                  //seconf parameter is in seconds
+                    tl.staggerFrom('section', 2, {
+                        opacity: 0,
+                        scale: .5,
+                        ease: Power2.easeOut
+                    }, 0.2)
 
-            //     // show .from() initially
-            //     tl.staggerFrom('h1, h2', .5, {
-            //     opacity: 0,
-            //     y: -40,
-            //     ease: Power2.easeInOut
-            //     }, 0.2, "-=2")
+                // show .from() initially
+                    tl.staggerFrom('h1, h2', .5, {
+                    opacity: 0,
+                    y: -40,
+                    ease: Power2.easeInOut
+                    }, 0.2, "-=2")
 
-            //     tl.staggerFrom('.anim-panel', 1, {
-            //     opacity: 0,
-            //     y: -40,
-            //     ease: Power2.easeInOut
-            //     }, 0.2, "-=1.5")
+                tl.staggerFrom('.anim-panel', 1, {
+                opacity: 0,
+                y: -40,
+                ease: Power2.easeInOut
+                }, 0.2, "-=1.5")
 
             }, false);
         });
